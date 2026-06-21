6d7c18bc45e506952ad47df2544c17b98e9d292f
mdeibrahim <mmeibrahim505@gmail.com>
2026-06-21 10:36:46 +0600

    first commit

---
+++ services/auth.ts
@@ -0,0 +1,9 @@
+import api from "./api";
+
+export const login = async (data: {
+  email: string;
+  password: string;
+}) => {
+  const response = await api.post("/auth/login/", data);
+  return response.data;
+};
